7b2ba77978155926e9356bac4f9cff9606b15056
Zhenyi He <snowymohehe@gmail.com>
2020-02-11 02:02:21 -0500

    :sparkles: integrate udp network funcionality to capsule hands scene

---
+++ UnitySample/Assets/Scripts/MessageHandler.cs
@@ -0,0 +1,42 @@
+﻿using Leap.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageHandler : MonoBehaviour
+{
+
+    public Network myUdpTcpNetwork;
+
+    public RigidHand leftHand, rightHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    string constructHandInfo()
+    {
+        string str = "";
+        if (leftHand.IsTracked) {
+            // valid data
+            Vector3 pos = leftHand.GetPalmPosition();
+            str += "left hand:" + pos.ToString("F3") + "----";
+        }
+        if (rightHand.IsTracked) {
+            // valid data
+            Vector3 pos = rightHand.GetPalmPosition();
+            str += "right hand:" + pos.ToString("F3");
+        }
+        return str;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string curMsg = constructHandInfo();
+        if(curMsg.Length > 0)
+            myUdpTcpNetwork.SendNetworkMessage("{\"leap_motion\":\"" + curMsg + "\"}");
+    }
+}
